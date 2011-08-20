bbcf9b04441642e6fc4df8d7761a1c35b66db812
Tatsuya Yamamoto <tatsuya@ymt2.net>
2011-08-20 15:20:41 +0900

    fix TypeError (when str is blank)

--- body.js
+++ body.js
@@ -1,9 +1,12 @@
 /*
+ * get bytecount from multibyte string
  * Author: Tatsuya Yamamoto
  */
 (function(){
-  if ( String.prototype.mstrbyte ) return;
+if ( String.prototype.mstrbyte ) {
   String.prototype.mstrbyte = function(){
-  var s = encodeURI(this);
-  return (s.length-s.match(/%/g).length*2);
+    var s = encodeURI(this);
+    var m = s.match(/%/g);
+    return m ? (s.length-m.length*2) : s.length;
+  };
 }})();
